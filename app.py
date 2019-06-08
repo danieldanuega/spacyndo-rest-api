244c6c4a60e0beb24e303b1f23d8319ccfd3c549
Daniel Go <32347454+danielgo13@users.noreply.github.com>
2019-06-08 11:06:57 +0700

    Add the api of SpaCy ID model
    Capable of tag, dependency parser, named entity recognizer (NER)

---
+++ app.py
@@ -0,0 +1,43 @@
+from flask import Flask, send_from_directory
+from flask_restful import Api, Resource, reqparse
+import spacy
+from spacy import displacy
+from pathlib import Path
+
+app = Flask(__name__)
+api = Api(app)
+
+# Load the model
+nlp = spacy.load('id_ud-tag-dep-ner-1.0.0')
+
+
+class Tasks(Resource):
+
+    def post(self, task):
+        req = reqparse.RequestParser()
+        req.add_argument("text")
+        args = req.parse_args()
+
+        doc = nlp(args["text"])
+
+        if(task == "dep"):
+            svg = displacy.render(doc, style="dep")
+            output_path = Path('graph/dep.svg')
+            output_path.open("w", encoding="utf-8").write(svg)
+            return send_from_directory('graph/','dep.svg')
+
+        elif(task == "ner"):
+            svg = displacy.render(doc, style="ent")
+            output_path = Path('graph/ner.svg')
+            output_path.open("w", encoding="utf-8").write(svg)
+            return send_from_directory('graph/','ner.svg')
+
+        else:
+            return "Task Not Found", 404
+
+
+api.add_resource(Tasks, "/tasks/<string:task>")
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
